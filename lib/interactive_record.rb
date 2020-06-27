3bd4af155e47ad4b41ffd53f14e366f3820377e8
Jenae Christensen <729jenae.christensen@gmail.com>
2020-06-27 19:44:38 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,5 +2,15 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class InteractiveRecord
-  
+  def self.find_by(attribute)
+    column_name = attribute.keys[0].to_s
+    value_name = attribute.values[0]
+
+    sql = <<-SQL
+      SELECT * FROM #{table_name}
+      WHERE #{column_name} = ?
+      SQL
+
+    DB[:conn].execute(sql, value_name);
+  end
 end
