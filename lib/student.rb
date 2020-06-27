2b79871cabe6f36b333a83f7b7f0e29fa81a14c7
Jenae Christensen <729jenae.christensen@gmail.com>
2020-06-27 19:17:54 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -10,6 +10,6 @@ class Student < InteractiveRecord
   
   def self.column_names 
     DB[:conn].results_as_hash = true 
-    sql = "pragma table_info("
+    sql = "pragma table_info('#{table_name}')"
   end
 end
