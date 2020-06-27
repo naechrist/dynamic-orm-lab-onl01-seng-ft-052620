d772ee9301f306f99460469158627a270948837a
Jenae Christensen <729jenae.christensen@gmail.com>
2020-06-27 19:19:29 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -14,7 +14,7 @@ class Student < InteractiveRecord
     table_info = DB[:conn].execute(sql)
     column_names = []
     table_info.each do |row|
-      column_names << row.
+      column_names << row["name"]
     end
   end
 end
